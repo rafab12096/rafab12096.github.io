a6235f325f1da4d8f407a8c49061ce262b7ffa67
rafab12096 <rafaela.a.bigi@gmail.com>
2024-08-08 18:48:32 -0400

    Create home.js

---
+++ home.js
@@ -0,0 +1,5 @@
+setInterval(hello, 5000);
+
+function hello() {
+  document.getElementById("right image").src="https://static.wixstatic.com/media/11062b_ab2368c186e743b583de0d0662e29950~mv2_d_5472_1992_s_2.png/v1/fill/w_1019,h_700,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/11062b_ab2368c186e743b583de0d0662e29950~mv2_d_5472_1992_s_2.png";
+}
